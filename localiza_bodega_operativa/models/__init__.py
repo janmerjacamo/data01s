821e0a454ce4c0e3435b6affc2ccf4f7b3f90e0a
Janmer Jacamo <jjacamo29@gmail.com>
2026-06-08 14:12:27 -0600

    Repositorio reiniciado

--- localiza_bodega_operativa/models/__init__.py
+++
@@ -1,7 +0,0 @@
-from . import product_template
-from . import localiza_puesto
-from . import localiza_articulo
-from . import localiza_gps
-from . import localiza_asignacion
-from . import localiza_importacion
-from . import localiza_dashboard
--- localiza_bodega_v3/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from . import puesto
-from . import articulo
-from . import asignacion
-from . import formulario
-from . import dashboard
